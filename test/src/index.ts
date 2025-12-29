7e0f37baf34ed8587935cae7b572290c33b8cc6f
Adam Patterson <adpatter2000@gmail.com>
2025-12-29 18:39:46 -0500

    Improve tests.

--- test/src/index.ts
+++ test/src/index.ts
@@ -1,20 +1,20 @@
-import { once } from 'node:events';
-import { createService, createWorkerPool } from 'scalability';
-import { Greeter } from './service.js';
+import { once } from "node:events";
+import { createService, createWorkerPool } from "scalability";
+import { Greeter } from "./service.js";
 
 export class MainThreadApp {
-    public n: number = 1;
-    getNumber(): number {
-        return this.n++;
-    }
+  public n = 1;
+  getNumber(): number {
+    return this.n++;
+  }
 }
 
 const workerPool = createWorkerPool({
-    workerCount: 10,
-    workerURL: './dist/service.js'
+  workerCount: 10,
+  workerURL: "./dist/service.js",
 });
 
-await once (workerPool, 'ready');
+await once(workerPool, "ready");
 
 const app = new MainThreadApp();
 const service = createService(workerPool);
@@ -24,14 +24,10 @@ const greeter = service.createServiceAPI<Greeter>();
 
 const results = [];
 for (let i = 0; i < 10; i++) {
-    results.push(greeter.greet('happy'));
+  results.push(greeter.greet("happy"));
 }
 
-console.time('test');
+console.time("test");
 const result = await Promise.all(results);
 console.log(result);
-console.timeEnd('test');
-
-
-
-
+console.timeEnd("test");
